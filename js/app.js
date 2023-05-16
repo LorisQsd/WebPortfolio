ba1e6be597414b05e0dbe9fc8b2689323df12a41
LorisQ <121116287+LorisQsd@users.noreply.github.com>
2023-05-16 13:27:07 +0200

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+const firstSubtitle = document.querySelector(".home__subtitle");
+const title = document.querySelector(".home__title");
+const secondSubtitle = document.querySelector(".home__subtitle:last-child");
+
+const titles = [firstSubtitle, title, secondSubtitle];
+
+window.addEventListener("load", displayTitles)
+
+function displayTitles(){
+    setTimeout(() => {
+        for(let i=0; i<titles.length; i++) {
+            setTimeout(()=> {
+                titles[i].classList.add("active");
+    
+            },i * 150)
+        }
+    }, 500);
+}
+
+const logo = document.querySelector(".home__logo")
+setTimeout(addActive(logo,1000))
+
+function addActive(element, time) {
+    setTimeout(()=> {element.classList.add("active")}, time)
+    
+}
+
+// INTERSECTION OBSERVER
+const main = document.querySelector("main")
+
+const sections = main.querySelectorAll("section")
+
+const options = {
+    rootMargin: "0px 0px -300px 0px"
+};
+
+const observer = new IntersectionObserver((entries) => {
+    for(const entry of entries){
+        if (entry.isIntersecting){
+            for(let i=0; i<entry.target.children.length; i++) {
+                setTimeout(()=> {
+                    entry.target.children[i].classList.add("active")
+        
+                },i * 500)
+            }
+        }
+    }
+
+}, options)
+
+sections.forEach(el => {observer.observe(el)})
